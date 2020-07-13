e9bc010b1ba5165edb0083a02918fe01c8f398ec
Tăng Đức Thịnh <thinhtd@vinbdi.org>
2020-07-14 00:37:40 +0700

    change main.go set paramer initialize function

--- main.go
+++ main.go
@@ -2,14 +2,13 @@
 
 package main
 
-import "os"
-
 func main(){
 	a := App{}
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"), 
-		os.Getenv("APP_DB_PASSWORD"), 
-		os.Getenv("APP_DB_NAME"),
+		//os.Getenv("APP_DB_USERNAME"),
+		//os.Getenv("APP_DB_PASSWORD"),
+		//os.Getenv("APP_DB_NAME"),
+		"postgres", "1", "postgres",
 	)
 
 	a.Run(":8010")
